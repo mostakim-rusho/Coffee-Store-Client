567db4c5e8ef6ea1c8cfc7dad5f057c154b637fa
rusho <anikhabib62@gmail.com>
2025-07-17 23:03:53 +0600

    added

--- src/component/SignUp.jsx
+++ src/component/SignUp.jsx
@@ -1,8 +1,38 @@
+import { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+
 const SignUp = () => {
+
+const {createUser} =useContext(AuthContext);
+
   const handleSignUp = (e) => {
     e.preventDefault();
     const form = e.target;
-    console.log(form.email.value, form.password.value);
+    const email =form.email.value;
+    const password = form.password.value;
+    console.log(email,password);
+    createUser(email,password)
+    .then(result=>{
+      console.log(result.user)
+      const user = {email};
+        fetch("http://localhost:5000/user",{
+        method:"POST",
+        headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        if(data.insertedId){
+            alert("succesfully done")
+        }
+    })
+    })
+    .catch(error=>{
+      console.log(error)
+    })
   };
 
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,51 +1,50 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from './App.jsx';
-import AddCoffee from './component/AddCoffee.jsx';
-import UpdateCoffee from './component/UpdateCoffee.jsx';
-import SignIn from './component/SignIn.jsx';
-import SignUp from './component/SignUp.jsx';
+import App from "./App.jsx";
+import AddCoffee from "./component/AddCoffee.jsx";
+import UpdateCoffee from "./component/UpdateCoffee.jsx";
+import SignIn from "./component/SignIn.jsx";
+import SignUp from "./component/SignUp.jsx";
+import AuthProvider from "./provider/AuthProvider.jsx";
 
 // Define your routes with loaders
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     loader: () => fetch("http://localhost:5000/coffee"),
     // 👇 This line is important
     fallbackElement: <p>Loading...</p>,
   },
   {
-    path: '/addcoffee',
+    path: "/addcoffee",
     element: <AddCoffee />,
   },
   {
-    path: '/updatecoffee/:id',
+    path: "/updatecoffee/:id",
     element: <UpdateCoffee />,
-    loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`),
+    loader: ({ params }) => fetch(`http://localhost:5000/coffee/${params.id}`),
     // 👇 This line is important
     fallbackElement: <p>Loading...</p>,
   },
-   {
-    path: '/signin',
-    element:  <SignIn></SignIn>,
+  {
+    path: "/signin",
+    element: <SignIn></SignIn>,
   },
-   {
-    path: '/signup',
-    element:  <SignUp></SignUp>,
+  {
+    path: "/signup",
+    element: <SignUp></SignUp>,
   },
 ]);
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 );
